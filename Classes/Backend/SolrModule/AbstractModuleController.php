6a231c1a9cdea67b4465fb39ae94584755ddc1a8
move:elevator (Jan Männig) <jma@move-elevator.de>
2015-08-14 15:48:16 +0200

    #22274 add fallback root page id

--- Classes/Backend/SolrModule/AbstractModuleController.php
+++ Classes/Backend/SolrModule/AbstractModuleController.php
@@ -2,27 +2,27 @@
 namespace ApacheSolrForTypo3\Solr\Backend\SolrModule;
 
 /***************************************************************
-*  Copyright notice
-*
-*  (c) 2013 Ingo Renner <ingo@typo3.org>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
+ *  Copyright notice
+ *
+ *  (c) 2013 Ingo Renner <ingo@typo3.org>
+ *  All rights reserved
+ *
+ *  This script is part of the TYPO3 project. The TYPO3 project is
+ *  free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  The GNU General Public License can be found at
+ *  http://www.gnu.org/copyleft/gpl.html.
+ *
+ *  This script is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  This copyright notice MUST APPEAR in all copies of the script!
+ ***************************************************************/
 
 use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 use TYPO3\CMS\Extbase\Mvc\Exception\NoSuchArgumentException;
@@ -135,10 +135,16 @@ abstract class AbstractModuleController extends ActionController implements Admi
 			$this->site = $site;
 		}
 
+		if (!$this->site instanceof \Tx_Solr_Site) {
+			$fallBackRootPageId = $this->getFallBackRootPageId();
+			$this->site = \Tx_Solr_Site::getSiteByPageId($fallBackRootPageId);
+		}
+
 		$this->request->setArgument('site', $this->site);
 
 		$moduleData = $this->moduleDataStorageService->loadModuleData();
 		$moduleData->setSite($this->site);
+
 		$this->moduleDataStorageService->persistModuleData($moduleData);
 	}
 
@@ -203,6 +209,25 @@ abstract class AbstractModuleController extends ActionController implements Admi
 			);
 			$this->controllerContext->getFlashMessageQueue()->enqueue($flashMessage);
 		}
+	}
+
+	/**
+	 * Get first rootpage id
+	 *
+	 * @return int
+	 */
+	protected function getFallBackRootPageId() {
+		$rootPageId = 0;
+		$rootPages = $GLOBALS['TYPO3_DB']->exec_SELECTgetRows(
+			'uid, title',
+			'pages',
+			'is_siteroot = 1 AND deleted = 0 AND hidden = 0 AND pid != -1'
+		);
+		if (count($rootPages) > 0) {
+			$rootPageId = $rootPages[0]['uid'];
+		}
+
+		return (int) $rootPageId;
 	}
 }
 
